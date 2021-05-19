6bfe510bdade0452eb38ecb617ccfa99caa64d12
Hannes Pétur Eggertsson <hannespetur@gmail.com>
2021-05-19 11:58:55 +0000

    moved include filesystem magic to utilities/filesystem.hpp

--- src/utilities/genotype.cpp
+++ src/utilities/genotype.cpp
@@ -11,6 +11,7 @@
 #include <graphtyper/typer/vcf.hpp>
 #include <graphtyper/typer/vcf_operations.hpp>
 #include <graphtyper/utilities/bamshrink.hpp>
+#include <graphtyper/utilities/filesystem.hpp> // std_filesystem -> std::filesystem/std::experimental::filesystem
 #include <graphtyper/utilities/genotype.hpp>
 #include <graphtyper/utilities/hts_parallel_reader.hpp>
 #include <graphtyper/utilities/options.hpp>
@@ -28,16 +29,6 @@
 #include <utility>
 #include <vector>
 
-// Source: https://askubuntu.com/questions/1256440/how-to-get-libstdc-with-c17-filesystem-headers-on-ubuntu-18-bionic
-#if __has_include(<filesystem>)
-  #include <filesystem>
-namespace std_filesystem = std::filesystem;
-#elif __has_include(<experimental/filesystem>)
-  #include <experimental/filesystem>
-namespace std_filesystem = std::experimental::filesystem;
-#else
-  #error "Missing the <filesystem> header."
-#endif
 
 namespace
 {
